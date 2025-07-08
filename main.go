733927eec2946e12db9d7786746f1941514da150
michaelessiet <emsaa2002@gmail.com>
2025-07-08 22:46:56 +0100

    fix: metadata and lockfile check

--- main.go
+++ main.go
@@ -357,18 +357,12 @@ func detectPackageManager(specifiedManager string) (PackageManagerInfo, error) {
 			return pm, nil
 		}
 	}
+
 	for key, pm := range supportedManagers {
 		// Check for lock files first
 		for _, lockFile := range pm.LockFiles {
 			if _, err := os.Stat(lockFile); err == nil {
 				color.Yellow("Found '%s' lock file, using %s.", lockFile, pm.Name)
-				return pm, nil
-			}
-		}
-		// Then check for metadata files
-		for _, metadataFile := range pm.MetadataFiles {
-			if _, err := os.Stat(metadataFile); err == nil {
-				color.Yellow("Found '%s' metadata file, using %s.", metadataFile, pm.Name)
 				return pm, nil
 			}
 		}
@@ -378,6 +372,17 @@ func detectPackageManager(specifiedManager string) (PackageManagerInfo, error) {
 			}
 		}
 	}
+
+	for _, pm := range supportedManagers {
+		// Check for metadata files like package.json, Podfile, etc.
+		for _, metaFile := range pm.MetadataFiles {
+			if _, err := os.Stat(metaFile); err == nil {
+				color.Yellow("Found '%s' metadata file, using %s.", metaFile, pm.Name)
+				return pm, nil
+			}
+		}
+	}
+
 	color.Yellow("No project file detected, falling back to system package manager.")
 	if _, err := exec.LookPath("brew"); err == nil {
 		return supportedManagers["brew"], nil
